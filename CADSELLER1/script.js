0e7df4cc1751f03618f0c9cf5104be203309b59e
detroit <pasavaskevic55@gmail.com>
2025-01-20 23:29:48 +0300

    Delete CADSELLER1 directory

--- CADSELLER1/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-document.getElementById('burger').addEventListener('click', function(){
-    document.querySelector('header').classList.toggle('open');
-})
-})
-
